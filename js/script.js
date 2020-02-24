3b9fe2f68d078b22968e0e6ad59580b706f42462
IlliaTemnov <59662784+IlliaTemnov@users.noreply.github.com>
2020-02-24 17:42:51 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 var link = document.querySelector(".buttonreserve");
 var popup = document.querySelector(".search-form");
 
+popup.classList.remove("lift-down")
+popup.classList.add("lift-up")
+
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.toggle("lift-down");
+    popup.classList.toggle("lift-down"); 
     popup.classList.toggle("lift-up");
+    
 });
